83ec56c3834fb4cff1526a376244305b9427b34b
Pedro Lucho <pedrolucho02@gmail.com>
2022-06-03 21:52:27 -0300

    Update ormconfig.prod.js

--- backend/ormconfig.prod.js
+++ backend/ormconfig.prod.js
@@ -1,17 +1,24 @@
 module.exports = {
-  "type": 'postgres',
-  "host": 'localhost',
-  "port": 5432,
-  "username": 'postgres',
-  "password": 'postgres',
-  "database": 'smartroad',
-  "entities":[
+  "type": process.env.NODE_ENV === 'prod' ? process.env.DB_TYPE : 'postgres',
+  "host": process.env.NODE_ENV === 'prod' ? process.env.DB_HOST : 'localhost',
+  "port": process.env.NODE_ENV === 'prod' ? process.env.DB_PORT : 5432,
+  "username": process.env.NODE_ENV === 'prod' ? process.env.DB_USER : 'postgres',
+  "password": process.env.NODE_ENV === 'prod' ? process.env.DB_PASS : 'postgres',
+  "database": process.env.NODE_ENV === 'prod' ? process.env.DB_DB : 'smartroad',
+  "entities": process.env.NODE_ENV === 'prod' ? [
     "./build/modules/**/infra/typeorm/entities/*.js"
+  ] : [
+    "./src/modules/**/infra/typeorm/entities/*.ts"
   ],
-  "migrations":[
+  "migrations": process.env.NODE_ENV === 'prod' ? [
     "./build/shared/infra/typeorm/migrations/*.js"
+  ] : [
+    "./src/shared/infra/typeorm/migrations/*.ts"
   ],
-  "cli":{
+  "cli": process.env.NODE_ENV === 'prod' ? {
     "migrationsDir": "./build/shared/infra/typeorm/migrations"
+  } : {
+    "migrationsDir": "./src/shared/infra/typeorm/migrations"
   }
 }
+
